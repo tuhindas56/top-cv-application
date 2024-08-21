9295cf7d9a5410848163d2ec4de69b6e896c7869
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-08-21 13:24:39 +0530

    feat: add state variables for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react"
 import "./styles/App.css"
 import Forms from "./components/Forms"
 import ResumePreview from "./components/ResumePreview"
 
+export type ReactFormInputEvent = React.FormEvent<HTMLInputElement>
+
 function App() {
+  const [identity, setIdentity] = useState({ name: "", role: "" })
+  const [contact, setContact] = useState({ email: "", phone: "" })
+
   return (
     <div className="main-container">
-      <Forms />
-      <ResumePreview />
+      <Forms identity={identity} setIdentity={setIdentity} contact={contact} setContact={setContact} />
+      <ResumePreview identity={identity} contact={contact} />
     </div>
   )
 }
